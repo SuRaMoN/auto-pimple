568fc5703f629327e4cfffafdcee8bc1a6f4592f
matthias <matthias@icontroller.be>
2014-01-13 01:08:46 +0100

    Added capibility to define custom services with aliases

--- src/AutoPimple/AutoPimple.php
+++ src/AutoPimple/AutoPimple.php
@@ -27,11 +27,26 @@ class AutoPimple extends Pimple
 	{
 		foreach($this->prefixMap as $to => $froms) {
 			foreach((array) $froms as $from) {
+				if($to != '' && strpos($id, $to) !== 0) {
+					continue;
+				}
+				$prefixedId = $from . substr($id, strlen($to));
+				if(array_key_exists($prefixedId, $this->values)) {
+					$this->alias($id, $prefixedId);
+					return true;
+				}
+			}
+		}
+
+		foreach($this->prefixMap as $to => $froms) {
+			foreach((array) $froms as $from) {
 				if($from != '' && strpos($id, $from) !== 0) {
 					continue;
 				}
 				$prefixedId = $to . substr($id, strlen($from));
-				$this->tryAutoRegisterServiceFromId($prefixedId);
+				if(! array_key_exists($prefixedId, $this->values)) {
+					$this->tryAutoRegisterServiceFromId($prefixedId);
+				}
 				if(array_key_exists($prefixedId, $this->values)) {
 					$this->alias($id, $prefixedId);
 					return true;
@@ -49,18 +64,22 @@ class AutoPimple extends Pimple
 
 	protected function camelize($name)
 	{
-		$name = preg_replace_callback('/(\.|_|^)(.)/', function($m) {
-			return ('.' == $m[1] ? '\\' : '') . strtoupper($m[2]);
+		$name = preg_replace_callback('/(\.|_|-|^)(.)/', function($m) {
+			return ('.' == $m[1] ? '\\' : '') . ('-' == $m[1] ? $m[2] : strtoupper($m[2]));
 		}, $name);
 		return $name;
 	}
 
 	protected function underscore($name)
 	{
-		$name = preg_replace_callback('/(?<!^|\\\\)[A-Z]/', function($m) {
+		$name = str_replace('\\', '.', $name);
+		$name = preg_replace_callback('/(?<!^|\.)[A-Z]/', function($m) {
 			return '_' . $m[0];
 		}, $name);
-		return strtolower(str_replace('\\', '.', $name));
+		$name = preg_replace_callback('/(^|\.)([a-z])/', function($m) {
+			return '-' . $m[2];
+		}, $name);
+		return strtolower($name);
 	}
 
 	protected function tryAutoRegisterServiceFromId($id)
